c6b6d93024fd2d4a2d869f1cebd5d59e88c37298
Mladen Mijatov <meaneye.rcf@gmail.com>
2018-01-23 15:53:54 +0100

    Integrated map with location pages.

--- site/scripts/map.js
+++ site/scripts/map.js
@@ -13,9 +13,11 @@ Site.Map = function() {
 	var self = this;
 
 	self.map = null;
+	self.popup = null;
 	self.area = null;
 	self.worker = null;
 	self.matches = null;
+	self.fixed = false;
 	self.points = new Array();
 	self.handler = new Object();
 
@@ -41,6 +43,11 @@ Site.Map = function() {
 		self.area.classList.add('area');
 		self.map.insertBefore(self.area, self.map.firstChild);
 
+		// create user interface for details list
+		self.popup = document.createElement('div');
+		self.popup.id = 'map-details';
+		self.map.parentNode.insertBefore(self.popup, self.map.nextNode);
+
 		// prepare points array
 		var points = self.map.querySelectorAll('figcaption a');
 		var point_data = new Array();
@@ -58,10 +65,62 @@ Site.Map = function() {
 
 			// store point based on it's vertical coordinate
 			self.points.push(point);
+
+			// attach event listeners
+			point.addEventListener('click', self.handler.point_click);
 		}
 
 		// configure worker
 		self.worker.postMessage([self.area.offsetWidth / 2, point_data]);
+	};
+
+	/**
+	 * Show detailed location information.
+	 */
+	self.show_details = function() {
+		// remove existing nodes
+		while (self.popup.firstChild)
+			self.popup.removeChild(self.popup.firstChild);
+
+		// update list content
+		for (var i=0, count=self.matches.length; i<count; i++) {
+			// duplicate matched point
+			var point = self.points[self.matches[i]];
+			var menu_item = point.cloneNode(true);
+
+			// add it to the list
+			self.popup.append(menu_item);
+		}
+
+		// update popup position
+		var area_x = self.map.offsetLeft + self.area.offsetLeft;
+		var area_y = self.map.offsetTop + self.area.offsetTop;
+		var pos_x = area_x - self.popup.offsetWidth - (self.area.offsetWidth / 2) - 10;
+		var pos_y = area_y - (self.popup.offsetHeight / 2);
+
+		self.popup.style.left = pos_x.toString() + 'px';
+		self.popup.style.top = pos_y.toString() + 'px';
+
+		// show popup
+		self.popup.classList.add('visible');
+
+		// set flag
+		self.fixed = true;
+	};
+
+	self.hide_details = function(params) {
+		// hide popup
+		self.popup.classList.remove('visible');
+
+		// unset flag
+		self.fixed = false;
+	};
+
+	/**
+	 * Handle clicking on point.
+	 */
+	self.handler.point_click = function(event) {
+		event.preventDefault();
 	};
 
 	/**
@@ -70,6 +129,9 @@ Site.Map = function() {
 	 * @param object event
 	 */
 	self.handler.map_mouse_enter = function(event) {
+		if (self.fixed)
+			return;
+
 		self.area.classList.add('active');
 	};
 
@@ -79,6 +141,9 @@ Site.Map = function() {
 	 * @param object event
 	 */
 	self.handler.map_mouse_leave = function(event) {
+		if (self.fixed)
+			return;
+
 		self.area.classList.remove('active');
 	};
 
@@ -88,15 +153,23 @@ Site.Map = function() {
 	 * @param object event
 	 */
 	self.handler.map_mouse_click = function(event) {
-		if (!event.ctrlKey)
-			return;
+		if (!event.ctrlKey) {
+			if (!self.fixed) {
+				// ensure we don't fix area for empty match list
+				if (self.matches != null && self.matches.length > 0)
+					self.show_details();
+			} else {
+				self.hide_details();
+			}
 
-		// calculate relative position of the pointer
-		var pos_x = event.clientX - self.map.offsetLeft;
-		var pos_y = event.clientY - self.map.offsetTop + window.scrollY;
+		} else {
+			// calculate relative position of the pointer
+			var pos_x = event.clientX - self.map.offsetLeft;
+			var pos_y = event.clientY - self.map.offsetTop + window.scrollY;
 
-		// show message
-		alert('Your `text_id` is: ' + pos_x + ' ' + pos_y);
+			// show message with clicked coordinates
+			alert('Your `text_id` is: ' + pos_x + ' ' + pos_y);
+		}
 	};
 
 	/**
@@ -105,6 +178,9 @@ Site.Map = function() {
 	 * @param object event
 	 */
 	self.handler.map_mouse_move = function(event) {
+		if (self.fixed)
+			return;
+
 		// calculate relative position of the pointer
 		var pos_x = event.clientX - self.map.offsetLeft;
 		var pos_y = event.clientY - self.map.offsetTop + window.scrollY;
